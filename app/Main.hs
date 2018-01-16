9a7cc93ad36fde27e9ed75e587ca4eaefa47fd70
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2018-01-16 23:24:17 +0200

    can read cases, pairs and print it out

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
 
-import Lib
+import System.IO (isEOF)
 
 main :: IO ()
-main = someFunc
+main = loop 1
+
+loop :: Integer -> IO ()
+loop caseNumber = do
+  eof <- isEOF
+  if eof
+    then return ()
+    else do
+      qty   <- getLine
+      pairs <- getPairs (read qty) []
+      putStrLn $ "Case " ++ show caseNumber ++ ": " ++ show pairs
+      loop $ caseNumber + 1
+
+getPairs :: Integer -> [(String, String)] -> IO [(String, String)]
+getPairs 0 acc          = return acc
+getPairs countdown acc  = do
+  rawPair <- getLine
+  let
+    [first, second] = words rawPair
+    pair            = (first, second)
+    in getPairs (countdown - 1) $ pair:acc
