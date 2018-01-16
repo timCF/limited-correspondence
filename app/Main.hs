c52a82e1440c2d5edb44d1b11ce7decc55fbbb41
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2018-01-17 00:06:09 +0200

    fix naming

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,10 @@ module Main where
 import System.IO (isEOF)
 
 main :: IO ()
-main = loop 1
+main = mainLoop 1
 
-loop :: Integer -> IO ()
-loop caseNumber = do
+mainLoop :: Integer -> IO ()
+mainLoop caseNumber = do
   eof <- isEOF
   if eof
     then return ()
@@ -14,7 +14,7 @@ loop caseNumber = do
       qty   <- getLine
       pairs <- getPairs (read qty) []
       putStrLn $ "Case " ++ show caseNumber ++ ": " ++ show pairs
-      loop $ caseNumber + 1
+      mainLoop $ caseNumber + 1
 
 getPairs :: Integer -> [(String, String)] -> IO [(String, String)]
 getPairs 0 acc          = return acc
