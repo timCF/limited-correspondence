b78921c3195d648b2527fa2d5af459b18d8acbd8
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2018-01-19 00:15:28 +0200

    solution works, now need to improve performance

--- app/Main.hs
+++ app/Main.hs
@@ -13,14 +13,49 @@ mainLoop caseNumber = do
     else do
       qty   <- getLine
       pairs <- getPairs (read qty) []
-      putStrLn $ "Case " ++ show caseNumber ++ ": " ++ show pairs
+      putStrLn $ "Case " ++ show caseNumber ++ ": " ++ chooseSolution (permutate pairs "" "" [])
       mainLoop $ caseNumber + 1
 
-getPairs :: Integer -> [(String, String)] -> IO [(String, String)]
+getPairs :: Integer -> [((String, String), Integer)] -> IO [((String, String), Integer)]
 getPairs 0 acc          = return acc
 getPairs countdown acc  = do
   rawPair <- getLine
   let
     [first, second] = words rawPair
-    pair            = (first, second)
+    pair            = ((first, second), countdown)
     in getPairs (countdown - 1) $ pair:acc
+
+permutate :: [((String, String), Integer)] -> String -> String -> [String] -> [String]
+permutate []    str1 str2 acc
+  | str1 == str2  = str1:acc
+  | otherwise     = acc
+permutate pairs str1 str2 acc =
+  if str1 == str2 && str1 /= ""
+    then str1:acc
+    else
+      foldl (generateFolder pairs str1 str2) acc pairs
+
+generateFolder :: [((String, String), Integer)] -> String -> String -> ([String] -> ((String, String), Integer) -> [String])
+generateFolder pairs str1 str2 = folder
+  where
+    folder acc ((sub1, sub2), index) =
+      let
+        newPairs = filter (\ ((_, _), xindex) -> xindex /= index) pairs
+        in permutate newPairs (str1 ++ sub1) (str2 ++ sub2) acc
+
+chooseSolution :: [String] -> String
+chooseSolution []         = "IMPOSSIBLE"
+chooseSolution [solution] = solution
+chooseSolution (x:xs)     =
+  let
+    (newLst, _) = foldl minimumByStrLen ([x], length x) xs
+    in minimum newLst
+  where
+    minimumByStrLen (acc, len) newStr =
+      let
+        newLen = length newStr
+      in
+        case compare newLen len of
+          LT -> ([newStr], newLen)
+          GT -> (acc, len)
+          EQ -> (newStr:acc, len)
