cd1af15e6e31c502fe93ab5a0911e9c0a248d78d
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2018-01-18 22:31:06 +0200

    fix input data format

--- app/Main.hs
+++ app/Main.hs
@@ -11,16 +11,15 @@ mainLoop caseNumber = do
   if eof
     then return ()
     else do
-      qty   <- getLine
-      pairs <- getPairs (read qty) []
-      putStrLn $ "Case " ++ show caseNumber ++ ": " ++ show pairs
+      qty          <- getLine
+      (lst1, lst2) <- getLists (read qty) [] []
+      putStrLn $ "Case " ++ show caseNumber ++ ": " ++ show (lst1, lst2)
       mainLoop $ caseNumber + 1
 
-getPairs :: Integer -> [(String, String)] -> IO [(String, String)]
-getPairs 0 acc          = return acc
-getPairs countdown acc  = do
+getLists :: Integer -> [String] -> [String] -> IO ([String], [String])
+getLists 0         xs1 xs2 = return (xs1, xs2)
+getLists countdown xs1 xs2 = do
   rawPair <- getLine
   let
-    [first, second] = words rawPair
-    pair            = (first, second)
-    in getPairs (countdown - 1) $ pair:acc
+    [x1, x2] = words rawPair
+    in getLists (countdown - 1) (x1:xs1) (x2:xs2)
