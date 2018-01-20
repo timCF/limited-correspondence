8ead4c33067be02ff693d00cccb18e4fcb455468
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2018-01-20 16:16:10 +0200

    refactoring - new Result data

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
 import System.IO (isEOF)
-import Debug.Trace
+
+-- data-related stuff
+
 data Buffer = Buffer {
   value   :: String,
   size    :: Int
@@ -13,15 +15,22 @@ data BufferPair = BufferPair {
   index   :: Int
 }
 
-defaultBuffer = Buffer {
-  value = "",
-  size  = 0
-}
+data Result = Result Buffer | IMPOSSIBLE
 
-impossibleBuffer = Buffer {
-  value = "IMPOSSIBLE",
-  size  = -1
-}
+instance Show Result where
+         show IMPOSSIBLE = "IMPOSSIBLE"
+         show (Result Buffer{value = value}) = value
+
+newBuffer :: String -> Buffer
+newBuffer value = Buffer {
+                    value = value,
+                    size  = length value
+                  }
+
+defaultBuffer :: Buffer
+defaultBuffer = newBuffer ""
+
+-- solution
 
 main :: IO ()
 main = mainLoop 1
@@ -34,7 +43,7 @@ mainLoop caseNumber = do
     else do
       qty   <- getLine
       pairs <- getPairs (read qty) []
-      putStrLn $ "Case " ++ show caseNumber ++ ": " ++ value (permutate pairs defaultBuffer defaultBuffer defaultBuffer impossibleBuffer)
+      putStrLn $ "Case " ++ show caseNumber ++ ": " ++ show (permutate pairs defaultBuffer defaultBuffer defaultBuffer IMPOSSIBLE)
       mainLoop $ caseNumber + 1
 
 getPairs :: Int -> [BufferPair] -> IO [BufferPair]
@@ -44,50 +53,47 @@ getPairs countdown accBuffer  = do
   let
     [first, second] = words rawPair
     pair = BufferPair {
-      buffer1 = Buffer {
-        value = first,
-        size  = length first
-      },
-      buffer2 = Buffer {
-        value = second,
-        size  = length second
-      },
+      buffer1 = newBuffer first,
+      buffer2 = newBuffer second,
       index   = countdown
     }
     in getPairs (countdown - 1) $ pair:accBuffer
 
-permutate :: [BufferPair] -> Buffer -> Buffer -> Buffer -> Buffer -> Buffer
-permutate [] Buffer{size = 0} Buffer{size = 0} tmpBuffer accBuffer =
-  chooseSolution tmpBuffer accBuffer
-permutate [] Buffer{} Buffer{} Buffer{} accBuffer =
-  accBuffer
-permutate pairs buff1 buff2 tmpBuffer accBuffer
-  | (size tmpBuffer > size accBuffer) && (size accBuffer /= -1) =
-      accBuffer
-  | (size tmpBuffer /= 0) && (size buff1 == 0) && (size buff2 == 0) =
-      chooseSolution tmpBuffer accBuffer
-  | otherwise =
+permutate :: [BufferPair] -> Buffer -> Buffer -> Buffer -> Result -> Result
+permutate [] Buffer{size = 0} Buffer{size = 0} tmpBuffer result =
+  chooseSolution tmpBuffer result
+permutate [] Buffer{} Buffer{} Buffer{} result =
+  result
+permutate pairs buff1 buff2 tmpBuffer result =
+  case result of
+    Result resultBuffer | size tmpBuffer > size resultBuffer ->
+      result
+    _ | (size tmpBuffer /= 0) && (size buff1 == 0) && (size buff2 == 0) ->
+      chooseSolution tmpBuffer result
+    _ ->
       case rmExtraPairs pairs buff1 buff2 of
-        [] -> accBuffer
-        goodPairs -> foldl folder accBuffer goodPairs
-  where
-    folder accBuffer BufferPair{buffer1 = buffer1, buffer2 = buffer2, index = pairIndex} =
-      let
-        tmpBuff1 = Buffer{value = value buff1 ++ value buffer1, size = size buff1 + size buffer1}
-        tmpBuff2 = Buffer{value = value buff2 ++ value buffer2, size = size buff2 + size buffer2}
-        size2get = min (size tmpBuff1) (size tmpBuff2)
-        (chunk1, newVal1) = smartSplit size2get tmpBuff1
-        (chunk2, newVal2) = smartSplit size2get tmpBuff2
-      in
-        if chunk1 /= chunk2
-          then accBuffer
-          else
-            let
-              newPairs      = filter (\ BufferPair{index = index} -> index /= pairIndex) pairs
-              newBuff1      = Buffer{value = newVal1, size = size tmpBuff1 - size2get}
-              newBuff2      = Buffer{value = newVal2, size = size tmpBuff2 - size2get}
-              newTmpBuffer  = Buffer{value = value tmpBuffer ++ chunk1, size = size tmpBuffer + size2get}
-              in permutate newPairs newBuff1 newBuff2 newTmpBuffer accBuffer
+        [] ->
+          result
+        goodPairs ->
+          foldl folder result goodPairs
+          where
+            folder result BufferPair{buffer1 = buffer1, buffer2 = buffer2, index = pairIndex} =
+              let
+                tmpBuff1 = Buffer{value = value buff1 ++ value buffer1, size = size buff1 + size buffer1}
+                tmpBuff2 = Buffer{value = value buff2 ++ value buffer2, size = size buff2 + size buffer2}
+                size2get = min (size tmpBuff1) (size tmpBuff2)
+                (chunk1, newVal1) = smartSplit size2get tmpBuff1
+                (chunk2, newVal2) = smartSplit size2get tmpBuff2
+              in
+                if chunk1 /= chunk2
+                  then result
+                  else
+                    let
+                      newPairs      = filter (\ BufferPair{index = index} -> index /= pairIndex) goodPairs
+                      newBuff1      = Buffer{value = newVal1, size = size tmpBuff1 - size2get}
+                      newBuff2      = Buffer{value = newVal2, size = size tmpBuff2 - size2get}
+                      newTmpBuffer  = Buffer{value = value tmpBuffer ++ chunk1, size = size tmpBuffer + size2get}
+                      in permutate newPairs newBuff1 newBuff2 newTmpBuffer result
 
 rmExtraPairs :: [BufferPair] -> Buffer -> Buffer -> [BufferPair]
 rmExtraPairs pairs Buffer{size = offset1} Buffer{size = offset2} =
@@ -106,13 +112,13 @@ smartSplit size2get Buffer{size = size, value = value} =
     then (value, "")
     else splitAt size2get value
 
-chooseSolution :: Buffer -> Buffer -> Buffer
-chooseSolution newBuffer Buffer{value = "IMPOSSIBLE"} = newBuffer
-chooseSolution newBuffer oldBuffer    =
+chooseSolution :: Buffer -> Result -> Result
+chooseSolution newBuffer IMPOSSIBLE = Result newBuffer
+chooseSolution newBuffer (Result oldBuffer) =
   case compare (size newBuffer) (size oldBuffer) of
-    LT -> newBuffer
-    GT -> oldBuffer
+    LT -> Result newBuffer
+    GT -> Result oldBuffer
     EQ ->
       case compare (value newBuffer) (value oldBuffer) of
-        LT -> newBuffer
-        _  -> oldBuffer
+        LT -> Result newBuffer
+        _  -> Result oldBuffer
