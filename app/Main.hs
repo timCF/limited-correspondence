ab53663328e549f5bdea71db457851d979b6ee84
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2018-01-20 03:46:53 +0200

    solution accepted

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import System.IO (isEOF)
-
+import Debug.Trace
 data Buffer = Buffer {
   value   :: String,
   size    :: Int
@@ -20,7 +20,7 @@ defaultBuffer = Buffer {
 
 impossibleBuffer = Buffer {
   value = "IMPOSSIBLE",
-  size  = 10
+  size  = -1
 }
 
 main :: IO ()
@@ -61,10 +61,15 @@ permutate [] Buffer{size = 0} Buffer{size = 0} tmpBuffer accBuffer =
   chooseSolution tmpBuffer accBuffer
 permutate [] Buffer{} Buffer{} Buffer{} accBuffer =
   accBuffer
-permutate pairs buff1 buff2 tmpBuffer accBuffer =
-  if (size tmpBuffer /= 0) && (size buff1 == 0) && (size buff2 == 0)
-    then chooseSolution tmpBuffer accBuffer
-    else foldl folder accBuffer pairs
+permutate pairs buff1 buff2 tmpBuffer accBuffer
+  | (size tmpBuffer > size accBuffer) && (size accBuffer /= -1) =
+      accBuffer
+  | (size tmpBuffer /= 0) && (size buff1 == 0) && (size buff2 == 0) =
+      chooseSolution tmpBuffer accBuffer
+  | otherwise =
+      case rmExtraPairs pairs buff1 buff2 of
+        [] -> accBuffer
+        goodPairs -> foldl folder accBuffer goodPairs
   where
     folder accBuffer BufferPair{buffer1 = buffer1, buffer2 = buffer2, index = pairIndex} =
       let
@@ -83,6 +88,17 @@ permutate pairs buff1 buff2 tmpBuffer accBuffer =
               newBuff2      = Buffer{value = newVal2, size = size tmpBuff2 - size2get}
               newTmpBuffer  = Buffer{value = value tmpBuffer ++ chunk1, size = size tmpBuffer + size2get}
               in permutate newPairs newBuff1 newBuff2 newTmpBuffer accBuffer
+
+rmExtraPairs :: [BufferPair] -> Buffer -> Buffer -> [BufferPair]
+rmExtraPairs pairs Buffer{size = offset1} Buffer{size = offset2} =
+  let
+    folder (acc1, acc2) BufferPair{buffer1 = Buffer{size = size1}, buffer2 = Buffer{size = size2}} =
+      (acc1 + size1, acc2 + size2)
+    (sum1, sum2) =
+      foldl folder (offset1, offset2) pairs
+    filterFun BufferPair{buffer1 = Buffer{size = size1}, buffer2 = Buffer{size = size2}} =
+       (size1 <= sum2) && (size2 <= sum1)
+    in filter filterFun pairs
 
 smartSplit :: Int -> Buffer -> (String, String)
 smartSplit size2get Buffer{size = size, value = value} =
